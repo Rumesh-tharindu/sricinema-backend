3738fd77a3cbf09a30bbbbb6e0be0639fa9f4b3b
Rumesh-tharindu <rumeshtharindu96@gmail.com>
2020-09-29 18:57:23 +0530

    connect with mysql database

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
-
 const app = express()
-
+const bodyParser=require('body-parser')
+const cors=require('cors')
+app.use(cors())
 const router=require('./route/router')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 app.use('/user',router)
+
 app.listen('5000',()=>{
     console.log("listening...")
 })
--- route/router.js
+++ route/router.js
@@ -1,8 +1,15 @@
 const express=require('express')
 const router=require('express').Router()
+const connection =require('../config')
+
 
 router.post('/register',(req,res)=>{
-    res.json({'username':'rumesh'})
+    const fullName=req.body.fullName
+    const email =req.body.email
+    const password=req.body.password
+    const phoneNumber=req.body.phone
+
+    res.json()
 })
 
 module.exports=router
